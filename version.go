3dd9d45ffbf4913429a730101a04d113964be5b3
sim <git@sgougeon.fr>
2025-01-28 16:32:10 +0100

    Comment legacy versionHandler

--- version.go
+++ version.go
@@ -12,6 +12,9 @@ type VHandler struct {
 	UnifiedPush UP `json:"unifiedpush"`
 }
 
+// UnifiedPush used to require returning {"unifiedpush":{"version":1}}
+// to allow application to discover UP endpoint. This is no longer the case.
+// We keep that request for legacy stuff.
 func versionHandler() func(http.ResponseWriter) {
 	b, err := json.Marshal(VHandler{UP{1}})
 	if err != nil {
